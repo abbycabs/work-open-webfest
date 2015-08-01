acee8921deb14a654389fde3d87abf5ae2739f42
Abigail Cabunoc <abigail.cabunoc@gmail.com>
2015-08-01 08:14:25 +0200

    timeline work

--- js/main.js
+++ js/main.js
@@ -8,11 +8,19 @@ var distance = { 0: 0,
                  7: 80,
                  8: 95 };
     timeline = document.querySelector('.tl'),
-    sr = timeline.querySelector('.tl-sr'),
-    dr = timeline.querySelector('.tl-dr'),
-    start = 4,
+    labels =
+      { sr: timeline.querySelector('.tl-sr'),
+        dr: timeline.querySelector('.tl-dr'),
+        credit: timeline.querySelector('.credit'),
+        doc: timeline.querySelector('.doc'),
+        sharing: timeline.querySelector('.sharing'),
+        participation: timeline.querySelector('.participation')
+      },
+    start = 3,
     progress = timeline.querySelector('.tl-progress');
 
+console.log(labels.credit);
+labels.credit.classList.add('active');
 // Full list of configuration options available at:
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
@@ -44,8 +52,13 @@ Reveal.addEventListener( 'slidechanged', function( event ) {
 function updateTimeline(h){
   var status = tlStatus(h);
   timeline.style.display = status.on ? 'block' : 'none';
-  (status.rev.sr) ? sr.classList.add('active') : sr.classList.remove('active');
-  (status.rev.dr) ? dr.classList.add('active') : dr.classList.remove('active');
+
+  for(var key in status.rev) {
+    if (status.rev.hasOwnProperty(key)) {
+      status.rev[key] ? labels[key].classList.add('active') : labels[key].classList.remove('active');
+    }
+  }
+
   progress.style.height = status.distance;
 }
 
@@ -53,7 +66,11 @@ function tlStatus(h) {
   return { on: (h>=start && h<(start+9)),
            rev: {
             'sr': (h>start && h<(start+5)) ? 'active' : '',
-            'dr': (h>(start+4)) ? 'active' : ''
+            'dr': (h>(start+4)) ? 'active' : '',
+            'credit': (h==(start+2) || h==(start+6)) ? 'active' : '',
+            'doc': (h==(start+2) || h==(start+6)) ? 'active' : '',
+            'sharing': (h==(start+3) || h==(start+7)) ? 'active' : '',
+            'participation': (h==(start+4) || h==(start+8)) ? 'active' : ''
            },
            distance: distance[h-start] + "%"
          };
